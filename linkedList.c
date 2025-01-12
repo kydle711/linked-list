2187cd95a6820a9f6db82a36631a24ad854d211f
Kyle Fuller <gitkyle711@gmail.com>
2025-01-11 21:37:11 -0500

    Linked List struct is working. printList function prints contents of list

--- linkedList.c
+++ linkedList.c
@@ -12,38 +12,34 @@ struct LinkedList{
 	};
 
 
-struct Node addValue(int new_value, int index, struct LinkedList *myList){
-	struct Node newNode;
-	newNode.value = new_value;
+void addNode(struct Node *newNode, struct LinkedList *myList){
 	if(myList->headNode == NULL){
-		newNode.nextNode = NULL;
-		newNode.prevNode = NULL;
-		myList->headNode = &newNode;
-		myList->tailNode = &newNode;
-		return newNode;
+		newNode->nextNode = NULL;
+		newNode->prevNode = NULL;
+		myList->headNode = newNode;
+		myList->tailNode = newNode;
+		return;
 		}
-	struct Node *last_node = myList->headNode;
-	struct Node *next_node = last_node->nextNode; 
-	int i = 1;
-	while(i < index && next_node != NULL){
-		*last_node = *next_node;
-		next_node = last_node->nextNode;
-		i++;
-		}
-	if(next_node == NULL){
-		last_node->nextNode = &newNode;
-		myList->tailNode = &newNode;
-		return newNode;
+	if(myList->headNode == myList->tailNode){
+		newNode->prevNode = myList->headNode;
+		newNode->nextNode = NULL;
+		myList->headNode->nextNode = newNode;
+		myList->tailNode = newNode;
+		return;
 		}
 	else{
-		newNode.nextNode = next_node;
-		newNode.prevNode = last_node;
-		return newNode;
+		struct Node* currentNode = myList->headNode;
+		while(currentNode->nextNode != NULL){
+			currentNode = currentNode->nextNode;
+			}
+		currentNode->nextNode = newNode;
+		myList->tailNode = newNode;
+		return;
 		}
 }
-//untested function
+// Prints the contents of the linked list
 void printList(struct LinkedList *a_list){
-	struct Node *currentNode = a_list->headNode;
+	struct Node* currentNode = a_list->headNode;
 	while(currentNode != NULL){
 		printf("%d\n", currentNode->value);
 		currentNode = currentNode->nextNode;
@@ -51,17 +47,17 @@ void printList(struct LinkedList *a_list){
 }
 
 int main(){
-	// unsure if additional assignments are working without debugging
 	struct LinkedList my_list;
-	struct Node node1 = addValue(10, 3, &my_list);
-	struct Node node2 = addValue(12, 1, &my_list);
-	struct Node node3 = addValue(48, 1, &my_list);
-	struct Node node4 = addValue(30, 1, &my_list);
-	struct Node node5 = addValue(29, 2, &my_list);
-	struct Node node6 = addValue(52, 1, &my_list);
+	struct Node node1 = {10};
+	addNode(&node1, &my_list);
+	struct Node node2 = {14};
+	addNode(&node2, &my_list);
+	struct Node node3 = {48};
+	addNode(&node3, &my_list);
+	struct Node node4 = {30};
+	addNode(&node4, &my_list);
 
-	//This call had a seg fault
-	//printList(&my_list);
+	printList(&my_list);
 	printf("HEAD NODE: %d\n", my_list.headNode->value);
-	printf("TAIL NODE: %d\n", my_list.tailNode->value); //<- didnt print correct num
+	printf("TAIL NODE: %d\n", my_list.tailNode->value);
 }
